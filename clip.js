44b530060adda22d34e4855cd0aa5a9a8f9741a9
RecordSB <totallynotquizlet@gmail.com>
2026-09-14 06:53:47 -0700

    Update clip.js to fix a url error

--- clip.js
+++ clip.js
@@ -288,7 +288,7 @@ window.downloadRecording = async function(btnEl, id, suggestedName) {
 };
 
 window.copyShareLink = function(el, name) {
-    const url = `https://recordsb.github.io/cc/clip.html/#${name}`;
+    const url = `https://recordsb.github.io/cc/clip.html#${name}`;
     navigator.clipboard.writeText(url).then(() => {
         showTooltip(el, 'Share Link Copied!');
     }).catch(err => {
